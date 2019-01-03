ffb14f12815461450212c4b405b3d7433b8ec2f6
jahnavi manchala <jahnavimanchala89@gmail.com>
2019-01-03 09:20:19 +0530

    commit
    sdf

---
+++ login.php
@@ -0,0 +1,204 @@
+<?php include("header.php"); ?>
+<style type="text/css">
+
+      
+      .carousel {
+        height: 60vh;
+      }
+	.font {
+		font-size:17px;
+	}
+.radiotext {
+  color: #213751;
+  font-size: 18px;
+}
+.customradio {
+  display: block;
+  position: relative;
+  padding-left: 30px;
+  margin-bottom: 0px;
+  cursor: pointer;
+  font-size: 18px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  margin-bottom:-100px;
+  margin-top:-100px;
+}
+.customradio input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+}
+.checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 22px;
+  width: 22px;
+  background-color: white;
+  border-radius: 50%;
+  border:1px solid #BEBEBE;
+  
+}
+.customradio:hover input ~ .checkmark {
+  background-color: transparent;
+}
+.customradio input:checked ~ .checkmark {
+  background-color: white;
+  border:1px solid #BEBEBE;
+}
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+.customradio input:checked ~ .checkmark:after {
+  display: block;
+}
+.customradio .checkmark:after {
+  top: 2px;
+  left: 2px;
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  background: #213751;
+}
+
+    </style>
+<!--Carousel Wrapper-->
+    <div  class="page-start" >
+    
+		<div class="">
+		<section >
+		<div class="container">
+			<div class="py-4 d-flex justify-content-center">
+				<div class="col-md-6">
+				<!-- Material form register -->
+	<div class="card">
+    
+   
+  
+    <h5 class="card-header  text-center py-4">
+        <strong>Login</strong>
+    </h5>	
+    <!--Card content-->
+	<div class="card-body px-lg-5 pt-0">
+	
+
+<div class="container">
+	<div class="row">
+		 <div class="col-md-4 col-sm-4 col-xs-12 form-group">
+		          <div class="form-check-inline">
+					<label class="customradio"><span class="radiotext" >Customer</span>
+					  <input type="radio" checked="checked" name="radio">
+					  <span class="checkmark"></span>
+					</label>  &nbsp;      
+					<label class="customradio"><span class="radiotext">Owner</span>
+					  <input type="radio" name="radio">
+					  <span class="checkmark"></span>
+					</label>&nbsp;
+					<label class="customradio"><span class="radiotext">Driver</span>
+					  <input type="radio"  name="radio">
+					  <span class="checkmark"></span>
+					</label> &nbsp;
+				</div>
+		  </div>
+	</div>
+</div>
+		
+		
+		
+	</div>
+	
+    <div class="card-body px-lg-5 pt-0">
+	
+		
+        <!-- Form -->
+		
+        <form class="text-center" style="color: #757575;">
+
+            <div class="form-row">
+                <div class="col">
+                    <!-- First name -->
+                    <div class="md-form">
+                        <input type="text" id="materialRegisterFormFirstName" class="form-control">
+                        <label for="materialRegisterFormFirstName">User Name</label>
+                    </div>
+                </div>
+                
+            </div>
+			<div class="form-row">
+            <div class="col">
+                    <!-- Last name -->
+                    <div class="md-form">
+                        <input type="password" id="materialRegisterFormLastName" class="form-control">
+                        <label for="materialRegisterFormLastName">Password</label>
+                    </div>
+                </div>
+
+           </div>
+
+            <!-- Sign up button -->
+            <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">Login</button>
+
+            <!-- Social register -->
+            <p>or Login with:</p>
+
+            <a type="button" class="btn-floating btn-fb btn-sm">
+                <i class="fa fa-facebook"></i>
+            </a>
+            <a type="button" class="btn-floating btn-tw btn-sm">
+                <i class="fa fa-twitter"></i>
+            </a>
+            <a type="button" class="btn-floating btn-gplus btn-sm">
+                <i class="fa fa-google-plus"></i>
+            </a>
+           
+
+        
+
+            <!-- Terms of service -->
+            <p>By clicking
+                <em>Sign up</em> you agree to our
+                <a href="" target="_blank">terms of service</a>
+
+        </form>
+		
+        <!-- Form -->
+   
+		
+    </div>
+
+</div>
+<!-- Material form register -->
+		
+				</div>
+			</div>
+			</div>
+			
+		</section>
+		
+		
+		</div>
+		</div>
+		<script>
+function openPage(pageName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+</script>
+<?php include("footer.php"); ?>
--- signup.php
+++ signup.php
@@ -5,29 +5,73 @@
       .carousel {
         height: 60vh;
       }
+	.tablink {
+  background-color:#00c851 ;
+  color: white;
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 14px 16px;
+  font-size: 17px; 
+}
+.tabcontent {
+  color: white;
+  display: none;
+  padding:0;
+  height: 100%;
+}
+.button {
+	width: 175px; 
+}
 
-
-    
+@media only screen and (max-width: 600px) {
+ .button1 {
+	width:105px;
+	
+}
+.button2 {
+	width:97px;
+}
+.button3 {
+	width:97px;
+	
+}
+}
+  
     </style>
 <!--Carousel Wrapper-->
     <div  class="page-start" >
     
 		<div class="">
-		<section>
+		<section >
 		<div class="container">
 			<div class="py-4 d-flex justify-content-center">
 				<div class="col-md-6">
 				<!-- Material form register -->
 	<div class="card">
-
+    
+   
+  
     <h5 class="card-header  text-center py-4">
         <strong>Sign up</strong>
-    </h5>
-
+    </h5>	
     <!--Card content-->
+	<div class="card-body px-lg-0 pt-0">
+	<div class="container">
+<div class="row">
+		<button class="tablink button" onclick="openPage('customer', this, '#213751')" id="defaultOpen">Customer</button>
+		<button class="tablink button" onclick="openPage('owner', this, '#213751')">Owner</button>
+		<button class="tablink button" onclick="openPage('driver', this, '#213751')">Driver</button>
+		</div>
+		</div>
+	</div>
+	
     <div class="card-body px-lg-5 pt-0">
-
+	
+		
         <!-- Form -->
+		<div id="owner" class="tabcontent">
         <form class="text-center" style="color: #757575;">
 
             <div class="form-row">
@@ -35,18 +79,43 @@
                     <!-- First name -->
                     <div class="md-form">
                         <input type="text" id="materialRegisterFormFirstName" class="form-control">
-                        <label for="materialRegisterFormFirstName">First name</label>
+                        <label for="materialRegisterFormFirstName">First Name</label>
                     </div>
                 </div>
                 <div class="col">
                     <!-- Last name -->
                     <div class="md-form">
                         <input type="email" id="materialRegisterFormLastName" class="form-control">
-                        <label for="materialRegisterFormLastName">Last name</label>
+                        <label for="materialRegisterFormLastName">Last Name</label>
                     </div>
                 </div>
             </div>
-
+			<div class="form-row">
+                <div class="col">
+                    <!-- First name -->
+                    <div class="md-form">
+                        <input type="text" id="materialRegisterFormFirstName" class="form-control">
+                        <label for="materialRegisterFormFirstName">Vehicle Type</label>
+                    </div>
+                </div>
+                <div class="col">
+                    <!-- Last name -->
+                    <div class="md-form">
+                        <input type="email" id="materialRegisterFormLastName" class="form-control">
+                        <label for="materialRegisterFormLastName">Vehicle No</label>
+                    </div>
+                </div>
+            </div>
+			<div class="form-row">
+                <div class="col">
+                    <!-- First name -->
+                    <div class="md-form">
+                        <input type="text" id="materialRegisterFormFirstName" class="form-control">
+                        <label for="materialRegisterFormFirstName">Pan Number </label>
+                    </div>
+                </div>
+                
+            </div>
             <!-- E-mail -->
             <div class="md-form mt-0">
                 <input type="email" id="materialRegisterFormEmail" class="form-control">
@@ -61,7 +130,12 @@
                     At least 8 characters and 1 digit
                 </small>
             </div>
-
+			<!--confirm password-->
+			 <div class="md-form">
+                <input type="password" id="materialRegisterFormPassword" class="form-control" aria-describedby="materialRegisterFormPasswordHelpBlock">
+                <label for="materialRegisterFormPassword">Confirm Password</label>
+                
+            </div>
             <!-- Phone number -->
             <div class="md-form">
                 <input type="password" id="materialRegisterFormPhone" class="form-control" aria-describedby="materialRegisterFormPhoneHelpBlock">
@@ -98,8 +172,187 @@
                 <a href="" target="_blank">terms of service</a>
 
         </form>
+		</div>
         <!-- Form -->
+   <!-- Form -->
+		<div id="customer" class="tabcontent">
+  
 
+        <form class="text-center" style="color: #757575;">
+
+            <div class="form-row">
+                <div class="col">
+                    <!-- First name -->
+                    <div class="md-form">
+                        <input type="text" id="materialRegisterFormFirstName" class="form-control">
+                        <label for="materialRegisterFormFirstName">First Name </label>
+                    </div>
+                </div>
+                <div class="col">
+                    <!-- Last name -->
+                    <div class="md-form">
+                        <input type="email" id="materialRegisterFormLastName" class="form-control">
+                        <label for="materialRegisterFormLastName">Last Name</label>
+                    </div>
+                </div>
+            </div>
+		
+            <!-- E-mail -->
+            <div class="md-form mt-0">
+                <input type="email" id="materialRegisterFormEmail" class="form-control">
+                <label for="materialRegisterFormEmail">E-mail</label>
+            </div>
+
+            <!-- Password -->
+            <div class="md-form">
+                <input type="password" id="materialRegisterFormPassword" class="form-control" aria-describedby="materialRegisterFormPasswordHelpBlock">
+                <label for="materialRegisterFormPassword">Password</label>
+                <small id="materialRegisterFormPasswordHelpBlock" class="form-text text-muted mb-4">
+                    At least 8 characters and 1 digit
+                </small>
+            </div>
+			<!--confirm password-->
+			 <div class="md-form">
+                <input type="password" id="materialRegisterFormPassword" class="form-control" aria-describedby="materialRegisterFormPasswordHelpBlock">
+                <label for="materialRegisterFormPassword">Confirm Password</label>
+                
+            </div>
+            <!-- Phone number -->
+            <div class="md-form">
+                <input type="password" id="materialRegisterFormPhone" class="form-control" aria-describedby="materialRegisterFormPhoneHelpBlock">
+                <label for="materialRegisterFormPhone">Phone number</label>
+                <small id="materialRegisterFormPhoneHelpBlock" class="form-text text-muted mb-4">
+                    Optional - for two step authentication
+                </small>
+            </div>
+
+           
+
+            <!-- Sign up button -->
+            <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">Sign in</button>
+
+            <!-- Social register -->
+            <p>or sign up with:</p>
+
+            <a type="button" class="btn-floating btn-fb btn-sm">
+                <i class="fa fa-facebook"></i>
+            </a>
+            <a type="button" class="btn-floating btn-tw btn-sm">
+                <i class="fa fa-twitter"></i>
+            </a>
+            <a type="button" class="btn-floating btn-gplus btn-sm">
+                <i class="fa fa-google-plus"></i>
+            </a>
+           
+
+        
+
+            <!-- Terms of service -->
+            <p>By clicking
+                <em>Sign up</em> you agree to our
+                <a href="" target="_blank">terms of service</a>
+
+        </form>
+		</div>
+        <!-- Form -->
+		<!-- Form -->
+		<div id="driver" class="tabcontent">
+  
+
+        <form class="text-center" style="color: #757575;">
+
+            <div class="form-row">
+                <div class="col">
+                    <!-- First name -->
+                    <div class="md-form">
+                        <input type="text" id="materialRegisterFormFirstName" class="form-control">
+                        <label for="materialRegisterFormFirstName">First Name </label>
+                    </div>
+                </div>
+                <div class="col">
+                    <!-- Last name -->
+                    <div class="md-form">
+                        <input type="email" id="materialRegisterFormLastName" class="form-control">
+                        <label for="materialRegisterFormLastName">Last Name</label>
+                    </div>
+                </div>
+            </div>
+			
+			 <div class="form-row">
+                <div class="col">
+                    <!-- First name -->
+                    <div class="md-form">
+                        <input type="text" id="materialRegisterFormFirstName" class="form-control">
+                        <label for="materialRegisterFormFirstName">License No</label>
+                    </div>
+                </div>
+                <div class="col">
+                    <!-- Last name -->
+                    <div class="md-form">
+                        <input type="email" id="materialRegisterFormLastName" class="form-control">
+                        <label for="materialRegisterFormLastName">Badge No</label>
+                    </div>
+                </div>
+            </div>
+			 
+            <!-- E-mail -->
+            <div class="md-form mt-0">
+                <input type="email" id="materialRegisterFormEmail" class="form-control">
+                <label for="materialRegisterFormEmail">E-mail</label>
+            </div>
+
+            <!-- Password -->
+            <div class="md-form">
+                <input type="password" id="materialRegisterFormPassword" class="form-control" aria-describedby="materialRegisterFormPasswordHelpBlock">
+                <label for="materialRegisterFormPassword">Password</label>
+                <small id="materialRegisterFormPasswordHelpBlock" class="form-text text-muted mb-4">
+                    At least 8 characters and 1 digit
+                </small>
+            </div>
+			<!--confirm password-->
+			 <div class="md-form">
+                <input type="password" id="materialRegisterFormPassword" class="form-control" aria-describedby="materialRegisterFormPasswordHelpBlock">
+                <label for="materialRegisterFormPassword">Confirm Password</label>
+                
+            </div>
+            <!-- Phone number -->
+            <div class="md-form">
+                <input type="password" id="materialRegisterFormPhone" class="form-control" aria-describedby="materialRegisterFormPhoneHelpBlock">
+                <label for="materialRegisterFormPhone">Phone number</label>
+                <small id="materialRegisterFormPhoneHelpBlock" class="form-text text-muted mb-4">
+                    Optional - for two step authentication
+                </small>
+            </div>
+
+           
+
+            <!-- Sign up button -->
+            <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">Sign in</button>
+
+            <!-- Social register -->
+            <p>or sign up with:</p>
+
+            <a type="button" class="btn-floating btn-fb btn-sm">
+                <i class="fa fa-facebook"></i>
+            </a>
+            <a type="button" class="btn-floating btn-tw btn-sm">
+                <i class="fa fa-twitter"></i>
+            </a>
+            <a type="button" class="btn-floating btn-gplus btn-sm">
+                <i class="fa fa-google-plus"></i>
+            </a>
+           
+
+        
+
+            <!-- Terms of service -->
+            <p>By clicking
+                <em>Sign up</em> you agree to our
+                <a href="" target="_blank">terms of service</a>
+
+        </form>
+		</div>
+        <!-- Form -->
     </div>
 
 </div>
@@ -114,4 +367,22 @@
 		
 		</div>
 		</div>
+		<script>
+function openPage(pageName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+</script>
 <?php include("footer.php"); ?>
