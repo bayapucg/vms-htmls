e3c4946555b784435f4e2312084e0b8d5238610d
jahnavi manchala <jahnavimanchala89@gmail.com>
2019-01-30 18:33:49 +0530

    commit
    changes

--- admin/header.php
+++ admin/header.php
@@ -23,6 +23,7 @@
   <link rel="stylesheet" href="css/AdminLTE.min.css">
   
   <link rel="stylesheet" href="css/_all-skins.min.css">
+  <link rel="stylesheet" href="css/vms.css">
   <!-- jQuery 2.2.3 -->
 <script src="js/jquery-2.2.3.min.js"></script>
 <script src="js/bootstrapValidator.min.js"></script>
@@ -55,58 +56,58 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </a>
-
-      <div class="navbar-custom-menu">
-        <ul class="nav navbar-nav">
-          <!-- Messages: style can be found in dropdown.less-->
-          <li class="dropdown messages-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <i class="fa fa-envelope-o"></i>
-              <span class="label label-success">4</span>
+		<div class="navbar-custom-menu1">
+        <ul class="nav navbar-nav ">
+          <li class="">
+            <a href="#" >
+             <p>VEHICLE MANAGEMNET SYSTEM PVT.LTD</p>  
             </a>
-            <ul class="dropdown-menu">
-              <li class="header">You have 4 messages</li>
-              <li>
-                <!-- inner menu: contains the actual data -->
-                <ul class="menu">
-                  <li><!-- start message -->
-                    <a href="#">
-                      <div class="pull-left">
-                        <img src="img/user.png" class="img-circle" alt="User Image">
-                      </div>
-                      <h4>
-                        Support Team
-                        <small><i class="fa fa-clock-o"></i> 5 mins</small>
-                      </h4>
-                      <p>Why not buy a new awesome theme?</p>
-                    </a>
-                  </li>
-                  <!-- end message -->
-                </ul>
-              </li>
-              <li class="footer"><a href="#">See All Messages</a></li>
-            </ul>
+          </li>
+          <li class="">
+            <a href="#" class="" >
+             <p>13456</p> 
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav ">
+          <!-- Messages: style can be found in dropdown.less-->
+          <li class="">
+            <a href="#" >
+              
+              <p>help</p>
+            </a>
+           
           </li>
           <!-- Notifications: style can be found in dropdown.less -->
-          <li class="dropdown notifications-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <i class="fa fa-bell-o"></i>
-              <span class="label label-warning">10</span>
+          <li class="">
+            <a href="#" >
+             
+              <p>video</p>
             </a>
-            <ul class="dropdown-menu">
-              <li class="header">You have 10 notifications</li>
-              <li>
-                <!-- inner menu: contains the actual data -->
-                <ul class="menu">
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="footer"><a href="#">View all</a></li>
-            </ul>
+            
+          </li>
+		   <li class="">
+            <a href="#" >
+             
+              <p>dummy</p>
+            </a>
+            
+          </li>
+		   <li class="">
+            <a href="#" >
+             
+              <p>dummy</p>
+            </a>
+            
+          </li>
+		   <li class="">
+            <a href="#" >
+             
+              <p>dummy</p>
+            </a>
+            
           </li>
           <!-- Tasks: style can be found in dropdown.less -->
           
@@ -114,7 +115,7 @@
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="img/user.png" class="user-image" alt="User Image">
-              <span class="hidden-xs">Customer 1</span>
+              <span class="hidden-xs">admin</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
