d782aeea76302457f4b0ed7fc511bf384aa8e5d0
bayapucg <bayapu.edu004@gmail.com>
2019-01-31 16:17:57 +0530

    commit
    design changes

--- admin/index.php
+++ admin/index.php
@@ -2,216 +2,148 @@
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header content-color">
-	<div class="Select_vms" style="" >
-      <div class="dropdown">
-    <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Select
+	<div class="row">
+	<div class=" col-md-3"  >
+		<select class="form-control" style="margin-top:5px;">
+			<option>radha</option>
+			<option>test1</option>
+			<option>qqq</option>
+			<option>ww</option>
+			<option>cccc</option>
+		</select>
+      <!--<div class="dropdown">
+    <button class="btn btn-default btn-block dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Select
     <span class="caret"></span></button>
     <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
       <li role="presentation"><a role="menuitem" tabindex="-1" href="#">customer</a></li>
       <li role="presentation"><a role="menuitem" tabindex="-1" href="#">owner</a></li>
       <li role="presentation"><a role="menuitem" tabindex="-1" href="#">driver</a></li> 
     </ul>
+  </div>-->
   </div>
-  </div>
-  <div style="float:right;" class="div_vms">
-      <ol class="breadcrumb">	
-  <li class="icon_size"><i class="fa fa-envelope" aria-hidden="true"></i>
-    <li class="icon_size"><i class="fa fa-bell-o" aria-hidden="true"></i></li>
-        <li class="icon_size"><i class="fa fa-cart-plus" aria-hidden="true"></i></li>
-        <li class="icon_size"><i class="fa fa-arrows" aria-hidden="true"></i></li>
-		<div class="search-container" >
-    <form action="#">
-      <input type="text"  name="search">
-      <button type="submit"><i class="fa fa-search"></i></button>
-    </form>
-  </div>
-      </ol>
-	  </div>
+  <div  class=" col-md-9">
+		<ul class="bottom-nav">	
+		  <li class="icon_size"><i class="fa fa-envelope" aria-hidden="true"></i></li>
+			<li class="icon_size"><i class="fa fa-bell-o" aria-hidden="true"></i></li>
+				<li class="icon_size"><i class="fa fa-cart-plus" aria-hidden="true"></i></li>
+				<li class="icon_size"><i class="fa fa-arrows" aria-hidden="true"></i></li>
+				<li >
+					<form action="#">
+					  <input type="text"  name="search">
+					  <button type="submit"><i class="fa fa-search"></i></button>
+					</form>
+				</li>
+		</ul>
+	</div>
+	</div>
     </section>
-	<!-- <section class="content-header content-color1">
-      <div class="" style="">
-    <i class="fa fa-tachometer" aria-hidden="true"></i>
-    <p>dashboard</p>
-  </div>
-      <ol class="breadcrumb">
-        <p>Rating</p>
-        <li class=""><p>2.5</p></li>
-        <li class="active"><i class="fa fa-heart" aria-hidden="true"></i></li>
-        <li class="active"><i class="fa fa-heart" aria-hidden="true"></i></li>
-        <li class="active"><i class="fa fa-heart" aria-hidden="true"></i></li>
-        <li class="active"><i class="fa fa-heart" aria-hidden="true"></i></li>
-        <li class="active"><i class="fa fa-heart-o" aria-hidden="true"></i></li>
-      </ol>
-    </section>-->
-    <!-- Main content -->
+	
     <section class="content">
       <!-- Info boxes -->
-      <div class="row card_row">
-	  <div class="clearfix"></div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <p class="vms_para vms_his"><i class="fa fa-history" aria-hidden="true">BOOKING HISTORY</i></p>
-		  <div class="col-lg-6 vms_bor">
-		  <div class="row">
-		  <div class="col-lg-4">10</div>
-		  <div class="col-lg-8"> 23 <i class="fa fa-caret-up" aria-hidden="true"></i><p>Load</p></div></div>
-		  </div>
-		  <div class="col-lg-6 vms_bor"><div class="row">
-		  <div class="col-lg-4">10</div>
-		  <div class="col-lg-8"> 23 <i class="fa fa-caret-down" aria-hidden="true"></i><p>Load</p></div></div></div>
-		  <div class="row">
-		  <div class="col-lg-5 ">01-01-2019</div>
-		  <div class="col-lg-7 ">vizag-bihar</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-5 ">06-01-2019</div>
-		  <div class="col-lg-7 ">chennai-hyderabad</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-5 ">05-04-2019</div>
-		  <div class="col-lg-7 ">vizag-hyderabad</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-5 ">08-07-2019</div>
-		  <div class="col-lg-7 ">ongole-vijayawada</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-5 ">09-11-2019</div>
-		  <div class="col-lg-7 ">vijayawada-vizag</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-5 ">09-11-2019</div>
-		  <div class="col-lg-7 ">vijayawada-vizag</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-4"></div>
-		  <div class="col-lg-4"></div>
-		  <div class="col-lg-4">
-		  <a href="#" class="vms_more">more</a></div></div>
-          <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <p class="vms_para vms_his1"><i class="fa fa-history" aria-hidden="true">PAYMENT HISTORY</i></p>
-		  <div class="col-lg-6 vms_bor">
-		  <div class="row">
-		  <p>Balance 10%</p>
-		  <p>INR 1000</p>
-		  </div></div>
-		  <div class="col-lg-6 vms_bor">
-		  <div class="row">
-		  <p>Paid 90%</p>
-		  <p>INR 9000</p>
-		  </div></div>
-		  <div class="row">
-		  <div class="col-lg-5 ">01-01-2019</div>
-		  <div class="col-lg-2 ">3500</div>
-		  <div class="col-lg-5 ">canara bank</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-5 ">01-01-2019</div>
-		  <div class="col-lg-2 ">3500</div>
-		  <div class="col-lg-5 ">canara bank</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-5 ">01-01-2019</div>
-		  <div class="col-lg-2 ">3500</div>
-		  <div class="col-lg-5 ">canara bank</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-5 ">01-01-2019</div>
-		  <div class="col-lg-2 ">3500</div>
-		  <div class="col-lg-5 ">canara bank</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-5 ">01-01-2019</div>
-		  <div class="col-lg-2 ">3500</div>
-		  <div class="col-lg-5 ">canara bank</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-4"></div>
-		  <div class="col-lg-4"></div>
-		  <div class="col-lg-4">
-		  <a href="#" class="vms_more">more</a></div></div>
-        </div>
-        <!-- /.col -->
-        <!-- fix for small devices only -->
-        <div class="clearfix visible-sm-block"></div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <p class="vms_para vms_his2"><i class="fa fa-history" aria-hidden="true">WALLET</i></p>
-		  <div class="col-lg-12 vms_bor">
-		  <div class="row">INR 30000/-
-		 </div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-6 ">01-03-2019</div>
-		  <div class="col-lg-6 ">vizag</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-6 ">01-03-2019</div>
-		  <div class="col-lg-6 ">vizag</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-6 ">01-03-2019</div>
-		  <div class="col-lg-6 ">vizag</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-6 ">01-03-2019</div>
-		  <div class="col-lg-6 ">vizag</div>
-		  </div>
-		  <div class="row">
-		 <div class="col-lg-6 ">01-03-2019</div>
-		  <div class="col-lg-6 ">vizag</div>
-		  </div>
-		  <div class="row">
-		 <div class="col-lg-6 ">01-03-2019</div>
-		  <div class="col-lg-6 ">vizag</div>
-		  </div>
-		  <div class="row">
-		 <div class="col-lg-6 ">01-03-2019</div>
-		  <div class="col-lg-6 ">vizag</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-4"></div>
-		  <div class="col-lg-4"></div>
-		  <div class="col-lg-4">
-		  <a href="#" class="vms_more">more</a></div></div>
-        </div>
-		<div class="col-md-3 col-sm-6 col-xs-12">
-          <p class="vms_para vms_his3"><i class="fa fa-history" aria-hidden="true">DUMMY</i></p>
-		  <div class="col-lg-6 vms_bor">
-		  <div class="row">
-		  <div class="col-lg-4">10</div>
-		  <div class="col-lg-8"> 23 <i class="fa fa-caret-up" aria-hidden="true"></i><p>Load</p></div></div>
-		  </div>
-		  <div class="col-lg-6 vms_bor"><div class="row">
-		  <div class="col-lg-4">10</div>
-		  <div class="col-lg-8"> 23 <i class="fa fa-caret-down" aria-hidden="true"></i><p>Load</p></div></div></div>
-		  <div class="row">
-		  <div class="col-lg-6 ">wdefrgthy</div>
-		  <div class="col-lg-6 ">dfgth</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-6 ">wdefrgthy</div>
-		  <div class="col-lg-6 ">dfgth</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-6 ">wdefrgthy</div>
-		  <div class="col-lg-6 ">dfgth</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-6 ">wdefrgthy</div>
-		  <div class="col-lg-6 ">dfgth</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-6 ">wdefrgthy</div>
-		  <div class="col-lg-6 ">dfgth</div>
-		  </div>
-		  <div class="row">
-		  <div class="col-lg-4"></div>
-		  <div class="col-lg-4"></div>
-		  <div class="col-lg-4">
-		  <a href="#" class="vms_more">more</a></div></div>
-        </div>
+      <div class="row ">
+	
+        <div class="col-md-3 col-sm-6 col-xs-12 ">
+		 <div class="panel panel-primary">
+            <div class="panel-heading">
+              <h3 class="panel-title text-center">BOOKING HISTORY</h3>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="  "> 
+					  <table class="table table-user-information">
+						<tbody>
+						  <tr>
+							<td>Name:</td>
+							<td>Owner</td>
+						  </tr>
+						  <tr>
+							<td>Vehicle Type:</td>
+							<td>Lorry</td>
+						  </tr>
+						</tbody>
+					  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12 ">
+		 <div class="panel panel-warning ">
+            <div class="panel-heading">
+              <h3 class="panel-title text-center">PAYMENT HISTORY</h3>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="  "> 
+					  <table class="table table-user-information">
+						<tbody>
+						  <tr>
+							<td>Name:</td>
+							<td>Owner</td>
+						  </tr>
+						  <tr>
+							<td>Vehicle Type:</td>
+							<td>Lorry</td>
+						  </tr>
+						</tbody>
+					  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12 ">
+		 <div class="panel panel-success">
+            <div class="panel-heading">
+              <h3 class="panel-title text-center">WALLET</h3>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="  "> 
+					  <table class="table table-user-information">
+						<tbody>
+						  <tr>
+							<td>Name:</td>
+							<td>Owner</td>
+						  </tr>
+						  <tr>
+							<td>Vehicle Type:</td>
+							<td>Lorry</td>
+						  </tr>
+						</tbody>
+					  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12 ">
+		 <div class="panel panel-danger">
+            <div class="panel-heading">
+              <h3 class="panel-title text-center">And more.</h3>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="  "> 
+					  <table class="table table-user-information">
+						<tbody>
+						  <tr>
+							<td>Name:</td>
+							<td>Owner</td>
+						  </tr>
+						  <tr>
+							<td>Vehicle Type:</td>
+							<td>Lorry</td>
+						  </tr>
+						</tbody>
+					  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+		</div>
+       
+		
 		</div>
 		<div class="row card_row">
         <!-- /.col -->
