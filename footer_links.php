406168c514b0412c6eadb771cb8837a01a491ca1
bayapucg <bayapu.edu004@gmail.com>
2019-03-27 16:46:26 +0530

    commt
    cahgnes

--- footer_links.php
+++ footer_links.php
@@ -1,9 +1,5 @@
-<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
-	
-    <!-- Bootstrap tooltips -->
-    <script type="text/javascript" src="js/popper.min.js"></script>
-    <!-- Bootstrap core JavaScript -->
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <script type="text/javascript" src="js/bootstrapValidator.js"></script>
     <!-- MDB core JavaScript -->
     <script type="text/javascript" src="js/mdb.min.js"></script>
     <script type="text/javascript" src="js/moment.min.js"></script>
