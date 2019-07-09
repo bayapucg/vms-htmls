9efcbc4483705e2e6032fa620ab822ac463ee752
bayapucg <bayapu.edu004@gmail.com>
2019-07-09 10:38:58 +0530

    commit
    design

--- modules/customer/view-contact.php
+++ modules/customer/view-contact.php
@@ -15,220 +15,62 @@
     </section>
 
     <section class="content">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="mt-0 mb-0 pull-left">(Company Name) Booking Summary</h3>
-                <span class="pull-right"><a href="contacts-list.php" class="btn btn-info btn-sm">Back</a></span>
-                <div class="clearfix">&nbsp;</div>
-            </div>
-            <div class="panel-body">
+      <div class="panel-body">
                     <form action="" method="">
                         <div class="row">
-                            <div class="col-md-12 b-vans">
-                                <div class="panel-group" id="accordion">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading">
-                                            <h4 class="panel-title ">
-                                                <div class="table-responsive">
-                                                    <table width="100%">
-                                                        <thead class="black white-text row">
-                                                            <tr>
-                                                                <th class="col-md-2">Vehicle Info</th>
-                                                                <th class="col-md-2">Capacity</th>
-                                                                <th class="col-md-2">Loading Date / Time</th>
-                                                                <th class="col-md-1">Duration</th>
-                                                                <th class="col-md-2">Rating</th>
-                                                                <th class="col-md-2">Price</th>
-                                                                <th class="col-md-1">Status</th>
-                                                            </tr>
-                                                        </thead>
-
-                                                    </table>
-                                                </div>
-                                            </h4>
-                                        </div>
-
-                                    </div>
-                                    <div class="panel-heading py-4" style="background:#ddd;position:relative;border:1px solid #ddd;">
-                                        <h4 class="panel-title vechiles-list">
-                                            <div class="table-responsive">
-                                                <table width="100%">
-
-                                                    <tbody class="row">
-                                                        <tr>
-                                                            <td class="bv-vechile-info col-md-2">
-                                                                <p class="bvv-number">15820</p>
-                                                                <div class="text-center">
-                                                                    <i class="fa fa-truck fa-5x"></i>
-                                                                    <br>
-                                                                    <p class="bvv-name">TATA 1109</p>
-                                                                </div>
-                                                            </td>
-                                                            <td class="bv-capacity col-md-2">
-                                                                <p><i class="fa fa-balance-scale"></i> 16 Ton</p>
-                                                                <p><i class="fa fa-arrows-h"></i> 18 fit</p>
-                                                            </td>
-                                                            <td class="bv-loading-dt col-md-2">
-                                                                <div class="media">
-                                                                    <i class="fa fa-calendar fa-2x mt-2"></i>
-                                                                    <div class="media-body">
-                                                                        <p class="">06:00 - 10:00 AM</p>
-                                                                        <p class="pt-0">02/01/1995</p>
-                                                                    </div>
-                                                                </div>
-                                                            </td>
-                                                            <td class="bv col-md-1">
-                                                                <p><i class="fa fa-clock-o"></i> 16 hrs</p>
-                                                            </td>
-                                                            <td class="bv-rating col-md-2">
-                                                                <p>4.5</p>
-                                                                <ul class="list-inline">
-                                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                                    <li class="list-inline-item"><i class="fa fa-star-half-o"></i></li>
-                                                                </ul>
-                                                                <p class="text-mute">359 Reviews</p>
-                                                                <p class="text-success">520 Load</p>
-                                                            </td>
-                                                            <td class="bv-price col-md-2">
-                                                                <h4>INR 2000.00</h4>
-                                                            </td>
-                                                            <td class="col-md-1">
-                                                                <span class="text-red">Pending</span>
-                                                            </td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                        </h4>
-                                        <div class="clearfix">&nbsp;</div>
-                                    </div>
+						<div class="col-xs-12">
+					    <div class="panel panel-default">
+					<div class="panel-heading">
+					<h3 class="mt-0 pull-left mb-0"> Contact View</h3>
+					<span class="pull-right"> <a class="btn btn-primary btn-xs " href="add-contact.php"> Add Contact</a></span>
+					<div class="clearfix">&nbsp;</div>
+				</div>
+               
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="">
+                            <table id="example1" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Company Name</th>
+                                        <th>Name of the Customer</th>
+                                        <th>Mobile Number</th>
+                                        <th>Email</th>
+                                        <th>GST Number</th>
+                                        <th>Map</th>
+                                        <th>Address</th>
+                                        <th>Saved address</th>
+                                     
+                                    </tr>
+                                </thead>
+                                <tbody>
                                     
-                                    <br>
+                                    <tr>
+                                        <td>Name of the company belong to</td>
+                                        <td>Customer Name</td>
+                                        <td>98xxxxxx20</td>
+                                        <td>example@gmail.com</td>
+                                        <td>653xxx122x22</td>
+                                        <td>Some Text</td>
+                                        <td>Address of the customer</td>
+                                        <td>Address he already saved before<br>
+                                            <hr>Address he already saved before</td>
+                                      
+                                    </tr>
                                     
-                                    <div class="panel-heading py-4" style="background:#ddd;position:relative;border:1px solid #ddd;">
-                                        <h4 class="panel-title vechiles-list">
-                                            <div class="table-responsive">
-                                                <table width="100%">
-
-                                                    <tbody class="row">
-                                                        <tr>
-                                                            <td class="bv-vechile-info col-md-2">
-                                                                <p class="bvv-number">15820</p>
-                                                                <div class="text-center">
-                                                                    <i class="fa fa-truck fa-5x"></i>
-                                                                    <br>
-                                                                    <p class="bvv-name">TATA 1109</p>
-                                                                </div>
-                                                            </td>
-                                                            <td class="bv-capacity col-md-2">
-                                                                <p><i class="fa fa-balance-scale"></i> 16 Ton</p>
-                                                                <p><i class="fa fa-arrows-h"></i> 18 fit</p>
-                                                            </td>
-                                                            <td class="bv-loading-dt col-md-2">
-                                                                <div class="media">
-                                                                    <i class="fa fa-calendar fa-2x mt-2"></i>
-                                                                    <div class="media-body">
-                                                                        <p class="">06:00 - 10:00 AM</p>
-                                                                        <p class="pt-0">02/01/1995</p>
-                                                                    </div>
-                                                                </div>
-                                                            </td>
-                                                            <td class="bv col-md-1">
-                                                                <p><i class="fa fa-clock-o"></i> 16 hrs</p>
-                                                            </td>
-                                                            <td class="bv-rating col-md-2">
-                                                                <p>4.5</p>
-                                                                <ul class="list-inline">
-                                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                                    <li class="list-inline-item"><i class="fa fa-star-half-o"></i></li>
-                                                                </ul>
-                                                                <p class="text-mute">359 Reviews</p>
-                                                                <p class="text-success">520 Load</p>
-                                                            </td>
-                                                            <td class="bv-price col-md-2">
-                                                                <h4>INR 2000.00</h4>
-                                                            </td>
-                                                            <td class="col-md-1">
-                                                                <span class="text-success font-weight-bold">Delivered</span>
-                                                            </td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                        </h4>
-                                        <div class="clearfix">&nbsp;</div>
-                                    </div>
-                                    
-                                    <br>
-                                    
-                                    <div class="panel-heading py-4" style="background:#ddd;position:relative;border:1px solid #ddd;">
-                                        <h4 class="panel-title vechiles-list">
-                                            <div class="table-responsive">
-                                                <table width="100%">
-
-                                                    <tbody class="row">
-                                                        <tr>
-                                                            <td class="bv-vechile-info col-md-2">
-                                                                <p class="bvv-number">15820</p>
-                                                                <div class="text-center">
-                                                                    <i class="fa fa-truck fa-5x"></i>
-                                                                    <br>
-                                                                    <p class="bvv-name">TATA 1109</p>
-                                                                </div>
-                                                            </td>
-                                                            <td class="bv-capacity col-md-2">
-                                                                <p><i class="fa fa-balance-scale"></i> 16 Ton</p>
-                                                                <p><i class="fa fa-arrows-h"></i> 18 fit</p>
-                                                            </td>
-                                                            <td class="bv-loading-dt col-md-2">
-                                                                <div class="media">
-                                                                    <i class="fa fa-calendar fa-2x mt-2"></i>
-                                                                    <div class="media-body">
-                                                                        <p class="">06:00 - 10:00 AM</p>
-                                                                        <p class="pt-0">02/01/1995</p>
-                                                                    </div>
-                                                                </div>
-                                                            </td>
-                                                            <td class="bv col-md-1">
-                                                                <p><i class="fa fa-clock-o"></i> 16 hrs</p>
-                                                            </td>
-                                                            <td class="bv-rating col-md-2">
-                                                                <p>4.5</p>
-                                                                <ul class="list-inline">
-                                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                                    <li class="list-inline-item"><i class="fa fa-star-half-o"></i></li>
-                                                                </ul>
-                                                                <p class="text-mute">359 Reviews</p>
-                                                                <p class="text-success">520 Load</p>
-                                                            </td>
-                                                            <td class="bv-price col-md-2">
-                                                                <h4>INR 2000.00</h4>
-                                                            </td>
-                                                            <td class="col-md-1">
-                                                                <span class="text-red ">Pending</span>
-                                                            </td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                        </h4>
-                                        <div class="clearfix">&nbsp;</div>
-                                    </div>
-                                </div>
-                            </div>
+									
+                                </tbody>
+                            </table>
                         </div>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+            </div>
+            <!-- /.col -->
+        </div>
                     </form>
             </div>
-        </div>
     </section>
     <!-- /.content -->
 </div>
